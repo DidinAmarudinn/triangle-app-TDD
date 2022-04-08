5b91c128154cb8da132b64599e76282ddf8ff6e9
didinamarudin07 <didin.a@bts.id>
2022-04-09 01:07:25 +0700

    initial commit

---
+++ lib/tirangle_app.dart
@@ -0,0 +1,22 @@
+String detectTriangle(num sideA, num sideB, num sideC){
+  final side = [sideA,sideB,sideC];
+  side.sort();
+  for(var e in side){
+    if(e < 1){
+      throw Exception();
+    }
+  }
+  if(side[0] + side[1] <= side[2]){
+    throw Exception("Inequal Triangle");
+  }
+
+  if(side[0] == side[1] && side[0] == side[2]){
+    return "Segitiga Sama Sisi";
+  }
+
+  if(side[0] == side[1] || side[1] == side[2]){
+    return "Segitiga Sama Kaki";
+  }
+
+  return "Segitiga Sembarang";
+}
